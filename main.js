c7e568a0e2aad96a72bda75620bd81a39febfb60
byteThePizza <faisal.abuomar@outlook.com>
2021-11-15 23:50:50 +0400

    commit

--- main.js
+++ main.js
@@ -1,80 +1,35 @@
-//scene where we will render our objects
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xdddddd);
-const container = document.getElementsByClassName("firstcontainer");
-//then we need camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
-    0.1,1000
-    );
 
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
+    
+     const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
+        0.1,1000
+        );
+    
+    
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    const light = new THREE.DirectionalLight( 0x040404, 0.5 );
-    const geometry = new THREE.BoxGeometry(2,2,2);
-    const material = new THREE.MeshBasicMaterial({color: 0x00fff0});
-    const landgeo = new THREE.BoxGeometry(20,0,20);
-    const landMaterial = new THREE.MeshBasicMaterial({color:0xffffff});
-    const land = new THREE.Mesh(landgeo,landMaterial);
-    //mesh create the cube which takes the shape and the color
-    const cube = new THREE.Mesh(geometry,material);
-    land.position.x=0;
-    land.position.y=-1;
-    land.position.z=0;
-    cube.position.x=0;
-    cube.position.z=0;
-    cube.position.y=0;
-    scene.add(cube);
-    scene.add(land);
-    light.position.set(3,3,3);
-    light.position.multiplyScalar(1.5);
-    light.castShadow=true;
-    scene.add(light);
-    camera.position.z=10;
-    camera.position.y=6;
-    camera.rotation.x=-0.6;
-    
-    document.addEventListener('keydown', function(e) {
-        
-        if (e.keyCode == '38') {
-            cube.position.z -=0.1;
-            cube.rotation.x -=0.1;
-        }
-        else if (e.keyCode == '40') {
-            cube.position.z +=0.1;
-            cube.rotation.x +=0.1;
-        }
-        else if (e.keyCode == '37') {
-            cube.position.x -=0.1;
-            cube.rotation.z +=0.1;
-        }
-        else if (e.keyCode == '39') {
-            cube.position.x +=0.1;
-            cube.rotation.z -=0.1;
-        }
-    })
-    
+    const sunGeometry = new THREE.SphereGeometry( 2, 30, 40 );
+    const sunMaterial = new THREE.MeshBasicMaterial({color:0xffff00});
+    const sun= new THREE.Mesh(sunGeometry,sunMaterial);
+    const earthGeo = new THREE.SphereGeometry( 1, 30, 40 );
+    const earthMat = new THREE.MeshBasicMaterial({color:0xffff55});
+    const earth = new THREE.Mesh(earthGeo,earthMat);
+    camera.position.z=8;
+    earth.position.x=4;
+    scene.add(sun);
+    scene.add(earth);
+    const rad = 0.1;
+    const axis = new THREE.Vector3(0.5,0,0.5);
 
-    function onMouseMove( event ) {
 
-        // calculate mouse position in normalized device coordinates
-        // (-1 to +1) for both components
-    
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-        cube.position.x=mouse.x;
-    }
 
-    if(cube.position.x > 20){
-        cube.position.x =0;
-    }
-    
-    function animate (){
-        requestAnimationFrame(animate);
-        renderer.render(scene,camera);
-    }
+function animate(){
+    requestAnimationFrame(animate);
+    earth.rotateY(Math.PI);
+    renderer.render(scene,camera);
+}
 
-    
-
-    animate();
-    
+animate();
--- main1.js
+++ main1.js
@@ -1,11 +1,104 @@
-const scene = THREE.Scene();
+//scene where we will render our objects
+const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xdddddd);
-
+const container = document.getElementsByClassName("firstcontainer");
+//then we need camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
     0.1,1000
     );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    const light = new THREE.DirectionalLight( 0x040404, 0.5 );
+    const geometry = new THREE.SphereGeometry(1,10,10);
+    const material = new THREE.MeshBasicMaterial({color: 0x00fff0});
+    const landgeo = new THREE.BoxGeometry(20,0,20);
+    const landMaterial = new THREE.MeshBasicMaterial({color:0xffffff});
+    const land = new THREE.Mesh(landgeo,landMaterial);
+    //mesh create the cube which takes the shape and the color
+    const cube = new THREE.Mesh(geometry,material);
+    land.position.x=0;
+    land.position.y=-1;
+    land.position.z=0;
+    cube.position.x=0;
+    cube.position.z=0;
+    cube.position.y=0;
+    scene.add(cube);
+    scene.add(land);
+    light.position.set(3,3,3);
+    light.position.multiplyScalar(1.5);
+    light.castShadow=true;
+    scene.add(light);
+    camera.position.z=10;
+    camera.position.y=6;
+    camera.rotation.x=-0.6;
+    
+    var Keys = {
+        up: false,
+        down: false,
+        left: false,
+        right: false
+    };
+    window.onkeydown = function(e){
+        var kc = e.keyCode;
+        e.preventDefault();
+   
+        if(kc === 37) Keys.left = true;
+        if(kc === 38) Keys.up = true;
+        if(kc === 39) Keys.right = true;
+        if(kc === 40) Keys.down = true;
+    };
+    
+    window.onkeyup = function(e){
+        var kc = e.keyCode;
+        e.preventDefault();
+   
+        if(kc === 37) Keys.left = false;
+        if(kc === 38) Keys.up = false;
+        if(kc === 39) Keys.right = false;
+        if(kc === 40) Keys.down = false;
+   };
+
+   function main() {
+    /* body */
+  
+      move();
+    if(cube.position.x > 10){
+        cube.position.x =10;
+    }else if(cube.position.x < -10){
+        cube.position.x = -10;
+    }
+  };
+
+
+   function move(){
+
+    if(Keys.up){
+        cube.position.z -= 0.1;
+    }
+
+    if(Keys.down){
+        cube.position.z += 0.1;
+    }
+
+    if(Keys.left) {
+        cube.position.x -= 0.1;
+    }
+
+    if(Keys.right){
+        cube.position.x += 0.1;
+    }
+}   
+    
+
+ 
+    
+    function animate (){
+        requestAnimationFrame(animate);
+        renderer.render(scene,camera);
+    }
+
+    setInterval(main, 20);
+    animate();
+    
